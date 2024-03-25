5b93a52147093f26a05a782465bc592496aba040
gustjr2457 <gustjr2457@naver.com>
2024-03-25 23:35:18 +0900

    최종

--- HW/HW04/main.js
+++ HW/HW04/main.js
@@ -1,19 +1,124 @@
-function drawNum() {
-    var canvas = document.getElementById("GameScreenCanvas");
-    var ctx = canvas.getContext("2d");
+//Canvas Element 불러오기
+var canvas = document.getElementById("GameScreenCanvas");
+var ctx = canvas.getContext("2d");
+var studentID = 202202034;
 
-    // 사용자 입력 받기
-    var studentID = document.getElementById("studentID").value;
-
-    // 폰트 및 스타일 설정
-    ctx.font = "bold 20px 'digital-7'";
-    ctx.fillStyle = "black";
-
-    // 캔버스에 학번 텍스트 그리기
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var startX = 10;
-    for (var i = 0; i < studentID.length; i++) {
-        ctx.fillText(studentID[i], startX, 30);
-        startX += 20; // 숫자 간격 조절
+function drawNum(num)
+{
+    var strID = studentID.toString();
+    for (var i = 1; i <= strID.length; i++)
+    {
+        var digit = parseInt(strID[i-1], 10);
+        switch (digit) {
+            case 0:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 20);
+                ctx.lineTo(i * 30, 60); 
+                ctx.lineTo(i * 30 - 20, 60); 
+                ctx.lineTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30, 20);
+                ctx.stroke();
+                break;
+            case 1:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 20); 
+                ctx.lineTo(i * 30, 60); 
+                ctx.stroke();
+                break;
+            case 2:
+                ctx.beginPath();
+                ctx.moveTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30, 20);
+                ctx.lineTo(i * 30, 40);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.lineTo(i * 30 - 20, 60);
+                ctx.lineTo(i * 30, 60);
+                ctx.stroke();
+                break;
+            case 3:
+                ctx.beginPath();
+                ctx.moveTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30, 20);
+                ctx.lineTo(i * 30, 40);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.moveTo(i * 30, 40);
+                ctx.lineTo(i * 30, 60);
+                ctx.lineTo(i * 30 - 20, 60);
+                ctx.stroke();
+                break;
+            case 4:
+                ctx.beginPath();
+                ctx.moveTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.lineTo(i * 30, 40);
+                ctx.moveTo(i * 30, 20);
+                ctx.lineTo(i * 30, 60);
+                ctx.stroke();
+                break;
+            case 5:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 20);
+                ctx.lineTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.lineTo(i * 30, 40);
+                ctx.lineTo(i * 30, 60);
+                ctx.lineTo(i * 30 - 20, 60);
+                ctx.stroke();
+                break;
+            case 6:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 20);
+                ctx.lineTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30 - 20, 60);
+                ctx.lineTo(i * 30, 60);
+                ctx.lineTo(i * 30, 40);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.stroke();
+                break;
+            case 7:
+                ctx.beginPath();
+                ctx.moveTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30, 20);
+                ctx.lineTo(i * 30, 60);
+                ctx.stroke();
+                break;
+            case 8:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 40);
+                ctx.lineTo(i * 30, 20);
+                ctx.lineTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.lineTo(i * 30, 40);
+                ctx.lineTo(i * 30, 60);
+                ctx.lineTo(i * 30 - 20, 60);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.stroke();
+                break;
+            case 9:
+                ctx.beginPath();
+                ctx.moveTo(i * 30, 40);
+                ctx.lineTo(i * 30 - 20, 40);
+                ctx.lineTo(i * 30 - 20, 20);
+                ctx.lineTo(i * 30, 20);
+                ctx.lineTo(i * 30, 60);
+                ctx.stroke();
+                break;
+        }
     }
+    
 }
+
+
+// (50,300) (974,300) magenta 3
+
+ctx.beginPath();
+ctx.moveTo(50,canvas.height/2);
+ctx.lineTo(canvas.width - 50, canvas.height/2);
+ctx.strokeStyle = "magenta";
+ctx.lineWidth = 3;
+ctx.stroke();
+ctx.closePath();
+
+
+
+drawNum(studentID);     //화면 우측 상단에 숫자 쓰기
