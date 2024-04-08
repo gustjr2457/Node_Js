444026d00e4e6ffc0a1e761ef37fc56ac9fb6a2e
y0ngs1ck24 <y0ngs1ck24@gmail.com>
2024-04-08 11:21:19 +0900

    .

--- HW/HW06/main.js
+++ HW/HW06/main.js
@@ -24,6 +24,7 @@ class HeartObject
         var colorIndex = Math.floor(Math.random()*7);
         ctx.fillStyle = colors[colorIndex];
         ctx.fill();
+        
         ctx.closePath();        
     }
 }
