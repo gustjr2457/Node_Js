ccbf2df80a8d0e17b1e4d2c68772f72c8aad73e4
y0ngs1ck24 <y0ngs1ck24@gmail.com>
2024-04-08 11:40:17 +0900

    나중에 해야지

--- HW/HW06/main.js
+++ HW/HW06/main.js
@@ -1,8 +1,13 @@
 //Canvas Element 불러오기
 var canvas = document.getElementById("GameScreenCanvas");
 var ctx = canvas.getContext("2d");
-
 var colors = ["whitepurple", "yellow", "red", "magenta", "pink", "cyan", "orange"];
+
+canvas.onmousemove = function(event){
+    const NowX = event.clientX - ctx.canvas.offsetLeft;
+    const NowY = event.clientY - ctx.canvas.offsetTop;
+    draw(NowX, NowY);
+}
 
 class HeartObject
 {
@@ -16,25 +21,24 @@ class HeartObject
     draw()
     {
         ctx.beginPath();
-        for (var angle = 0; angle < Math.PI * 2; angle += 0.01) {
-        var x = this.positionX + this.radius * Math.cos(angle);
-        var y = this.positionY + this.radius * Math.sin(angle);
-        ctx.lineTo(x, y);
+        for (var angle = 0; angle < Math.PI; angle += 0.01) {
+            var x = this.positionX + 50 * -Math.cos(angle);
+            var y = this.positionY + 50 * -Math.sin(angle);
+            ctx.lineTo(x, y);
         }
         var colorIndex = Math.floor(Math.random()*7);
         ctx.fillStyle = colors[colorIndex];
         ctx.fill();
-        
-        ctx.closePath();        
+        ctx.closePath();
     }
 }
 
 
 
 
-function draw()
+function draw(NowX, NowY)
 {
-    var Circle = new HeartObject(Math.random()*6%6,Math.random()*100,Math.random()*500,Math.random()*500);
+    var Circle = new HeartObject(Math.random()*6%6,Math.random()*100,NowX,NowY);
     Circle.draw();
     requestAnimationFrame(draw);
 }
